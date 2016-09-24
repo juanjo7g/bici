b30caa4890ce66a66d3b6dfc4ed0a9019cf287ef
root <tinez7g@gmail.com>
2016-09-24 14:58:10 -0500

    Se agrega vista principal, se agrega el mapa de google y se conecta al api de google

---
+++ app/src/main/java/com/ihdn/bici/MyMapFragment.java
@@ -0,0 +1,46 @@
+package com.ihdn.bici;
+/**
+ * Created by root on 24/09/16.
+ */
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MyMapFragment extends Fragment implements OnMapReadyCallback {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_gmaps, container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        MapFragment fragment = (MapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        fragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        LatLng marker = new LatLng(6.265227, -75.568203);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker, 13));
+
+        googleMap.addMarker(new MarkerOptions().title("Hello Google Maps!").position(marker));
+    }
+}
